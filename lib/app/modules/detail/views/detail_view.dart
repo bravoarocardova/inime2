b9474a7d74b9e0b0ab5f1b927c18a2ed02b7e7e5
Bravo Aro Cardova <bravoarocardova@gmail.com>
2022-08-18 20:25:53 +0700

    change ui detail

--- lib/app/modules/detail/views/detail_view.dart
+++ lib/app/modules/detail/views/detail_view.dart
@@ -16,75 +16,79 @@ class DetailView extends GetView<DetailController> {
       body: SafeArea(
           child: Stack(
         children: [
-          Center(
-            child: SingleChildScrollView(
-              child: controller.obx(
-                  (data) => Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          CachedNetworkImage(
-                            imageUrl: (data?.attributes?.coverImage != null)
-                                ? data?.attributes?.coverImage?.small as String
-                                : data?.attributes?.posterImage?.medium
-                                    as String,
-                            placeholder: (context, url) =>
-                                const CircularProgressIndicator(),
-                            errorWidget: (context, url, error) => Column(
+          controller.obx(
+              (data) => SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CachedNetworkImage(
+                          imageUrl: (data?.attributes?.coverImage != null)
+                              ? data?.attributes?.coverImage?.small as String
+                              : data?.attributes?.posterImage?.medium as String,
+                          placeholder: (context, url) =>
+                              const CircularProgressIndicator(),
+                          errorWidget: (context, url, error) => Container(
+                            decoration: const BoxDecoration(color: Colors.grey),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: const [
-                                Text('WHOOPSS....'),
+                                Text(
+                                  'WHOOPSS....',
+                                  textAlign: TextAlign.center,
+                                ),
                                 Icon(Icons.error)
                               ],
                             ),
-                            height: 250,
-                            width: double.infinity,
-                            fit: BoxFit.cover,
                           ),
-                          Container(
-                            margin: const EdgeInsets.all(16),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  data?.attributes?.canonicalTitle as String,
-                                  style: const TextStyle(
-                                    fontSize: 24,
+                          height: 250,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(16),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                data?.attributes?.canonicalTitle as String,
+                                style: const TextStyle(
+                                  fontSize: 24,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 8,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                      "${data?.attributes?.ageRatingGuide ?? ''} (${data?.attributes?.ageRating ?? 'No Age Rating'})"),
+                                  Text(data?.attributes?.subtype as String),
+                                  Row(
+                                    children: [
+                                      const Icon(Icons.star_border),
+                                      Text(data?.attributes?.averageRating ??
+                                          "No Rating")
+                                    ],
                                   ),
-                                ),
-                                const SizedBox(
-                                  height: 8,
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                        "${data?.attributes?.ageRatingGuide ?? ''} (${data?.attributes?.ageRating ?? 'No Age Rating'})"),
-                                    Text(data?.attributes?.subtype as String),
-                                    Row(
-                                      children: [
-                                        const Icon(Icons.star_border),
-                                        Text(data?.attributes?.averageRating ??
-                                            "No Rating")
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(
-                                  height: 8,
-                                ),
-                                Text(data?.attributes?.synopsis as String)
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                  onError: ((error) => Text('$error')),
-                  onLoading: const Loading()),
-            ),
-          ),
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 8,
+                              ),
+                              Text(data?.attributes?.synopsis as String)
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+              onError: ((error) => Text('$error')),
+              onLoading: const Center(child: Loading())),
           const BackButtonWidget()
         ],
       )),
